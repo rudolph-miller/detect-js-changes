50ac5a7ac1503aea9948c30d508bba937194d0ef
Rudolph-Miller <chopsticks.tk.ppfm@gmail.com>
2016-01-27 12:24:02 +0900

    Parse config file

--- main.go
+++ main.go
@@ -1,9 +1,28 @@
 package main
 
 import (
+	"fmt"
 	"github.com/codegangsta/cli"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
 	"os"
 )
+
+type Config struct {
+	Urls []string
+}
+
+func getConfig(file string, env string) *Config {
+	data, _ := ioutil.ReadFile(file)
+	parsed := make(map[string]Config)
+	err := yaml.Unmarshal(data, &parsed)
+	if err != nil {
+		fmt.Println("Parse error")
+		os.Exit(1)
+	}
+	config := parsed[env]
+	return &config
+}
 
 func main() {
 	app := cli.NewApp()
@@ -12,13 +31,21 @@ func main() {
 	app.Version = "0.0.1"
 
 	var env string
+	var configFile string
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
 			Name:        "env, e",
 			Usage:       "env for cofig file",
 			EnvVar:      "ENV",
+			Value:       "default",
 			Destination: &env,
+		},
+		cli.StringFlag{
+			Name:        "config, c",
+			Usage:       "config file",
+			Value:       "detect_config.yml",
+			Destination: &configFile,
 		},
 	}
 
@@ -34,6 +61,9 @@ func main() {
 			Name:  "download",
 			Usage: "downloads JS files",
 			Action: func(c *cli.Context) {
+				config := getConfig(configFile, env)
+				urls := config.Urls
+				println(urls)
 				println("download JS files")
 			},
 		},
